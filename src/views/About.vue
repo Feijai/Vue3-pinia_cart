5666ecee677e70df04ffa44bbb7f67ff60a45922
altuve <chickbooman@yahoo.com.tw>
2023-01-30 18:12:17 +0800

    完成

---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ name }},{{ getUserName }},{{ wallet }}
+  </div>
+  <button type="button" @click="updateName">click</button>
+</template>
+
+<script>
+import { mapState, mapActions } from "pinia";
+import userStore from "@/stores/user";
+
+// compositioin API
+// optionAPI
+export default {
+  computed: {
+    // 1. store
+    // 2. 要帶入的state,getters
+    ...mapState(userStore, ["name", "getUserName", "wallet"]),
+  },
+  methods: {
+    ...mapActions(userStore, ["updateName"]),
+  },
+};
+</script>
